4984d3d18db7008a1ea0ec247c32ad843767e90a
なぎさ <38747158+sgt-nagisa@users.noreply.github.com>
2018-05-06 04:29:33 +0900

    ABC096D

---
+++ ABC096D.py
@@ -0,0 +1,16 @@
+import math
+
+N = int(input())
+P = []
+
+for k in range(2,55555):
+    f = 1
+    for l in range(2,int(math.sqrt(k))+1):
+        if k%l==0:
+            f = 0
+    if f and k%5==1:
+        P.append(k)
+    if len(P)>=N:
+        break
+
+print(" ".join(map(str,P)))
