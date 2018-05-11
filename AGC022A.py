52a9be8aa959e8d19922ceeb6d5e817f8a962af0
なぎさ <38747158+sgt-nagisa@users.noreply.github.com>
2018-05-12 04:48:27 +0900

    what

---
+++ AGC022A.py
@@ -0,0 +1,31 @@
+S = input()
+T = "abcdefghijklmnopqrstuvwxyz"
+A = [str(x) for x in T]
+
+if len(S) != 26:
+    for l in S:
+        A.remove(l)
+    print(S+A[0])
+    exit()
+
+if S == T[::-1]:
+    print(-1)
+    exit(0)
+
+U = []
+for k in range(1,26):
+    if S[-k-1] > S[-k]:
+        U.append(S[-k-1])
+        U.append(S[-k])
+    else:
+        U.append(S[-k-1])
+        U.append(S[-k])
+        t = S[-k-1]
+        S = S[:-k-1]
+        break
+
+U.sort()
+for l in U:
+    if t < l:
+        print(S+l)
+        exit(0)
